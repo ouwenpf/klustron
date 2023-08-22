a4b305636603d9303c471a2705c371db122f8347
ouwenpf <ouwenpf@gmail.com>
2023-08-22 18:43:55 +0800

    20230822

---
+++ install_klustron/install_klustron.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+COL_START='\e['
+COL_END='\e[0m'
+RED='31m'
+GREEN='32m'
+YELLOW='33m'
+config_json=$1
+user_passwd=($2 $3)
+VERSION=1.2.1
+
+if [[ ! -s $config_json ]]  ||  [[ $# -ne 3 ]]; then
+	echo -e "$COL_START${RED}Usage $0 args file_json $COL_END"
+  exit
+  
+elif ! nc -z  www.kunlunbase.com  80  &>/dev/null ;then   
+  echo  -e "$COL_START$RED当前主机网络异常$COL_END"
+  exit
+
+elif ! command -v jq &>/dev/null  ;then
+  echo  -e "$COL_START$RED正在下载jq命令 $COL_END"
+  
+  if [ -f "/etc/os-release" ]; then
+    source /etc/os-release
+    
+      if [[ "$ID" == "ubuntu" ]]; then
+        echo  -e "$COL_START${GREEN}OS is Ubuntu$COL_END"
+        sudo apt-get install -y jq &&  mdc=$(jq  '.node_manager.nodes[0].dc'  $config_json 2>/dev/null) 
+        
+      elif [[ "$ID" == "centos" ]]; then
+        echo  -e "$COL_START${GREEN}OS is CentOS$COL_END"
+        sudo yum install -y jq  &>/dev/null  &&  mdc=$(jq  '.node_manager.nodes[0].dc'  $config_json 2>/dev/null) 
+        
+      else
+        echo  -e "$COL_START$RED未知系统$COL_END"
+        exit
+
+      fi
+    
+  else
+      echo "os-release文件不存,未知系统"
+	    exit
+  fi
+  
+
+elif ! jq  '.'  $config_json &>/dev/null;then
+  echo -e "$COL_START$RED$config_json syntax error$COL_END"
+  exit 
+  
+<<!
+elif [[ $(echo `pwd`|grep 'cloudnative/cluster$'|wc -l)  -ne 1 ]]; then
+	echo  -e "$COL_START$RED请确保当前$0脚本在../cloudnative/cluster目录下 $COL_END"
+	exit 
+!
+
+
+elif ! jq  '.machines[].user'  $config_json |sed 's/null/"kunlun"/g'|sort -rn|uniq|egrep   -wq  "$USER";then
+  echo -e  "$COL_START$RED$config_json中不存在用户:$USER,请到对应的用户下执行脚本$COL_END"
+  exit  
+
+
+else
+	ip_list=($(jq  '.machines[].ip'  $config_json |xargs))
+	user_list=($(jq  '.machines[].user'  $config_json |xargs))
+	basedir_list=($(jq  '.machines[].basedir'  $config_json |xargs))
+	sshport_list=($(jq  '.machines[].sshport'  $config_json |xargs))
+  mdc=$(jq  '.node_manager.nodes[0].dc'  $config_json 2>/dev/null) 
+  
+
+fi
+
+
+check_sshport(){
+
+for i in  $(seq 0 $((${#ip_list[*]}-1)))
+do
+
+
+    if [[ "${user_list[$i]}" == "null" ]];then
+      user_list[$i]="kunlun"
+    fi 
+   
+    if [[ "${basedir_list[$i]}" == "null" ]];then
+      basedir_list[$i]="/kunlun"
+      
+    fi
+ 
+    if [[ "${sshport_list[$i]}" == "null" ]];then
+      sshport_list[$i]=22
+    fi
+	    
+    if ! nc -z  ${ip_list[$i]} ${sshport_list[$i]};then 
+      echo -e "$COL_START${RED}主机${ip_list[$i]}端口${sshport_list[$i]}有异常,无法连接,请检查网络$COL_END" 
+      let count_sshport++
+      continue  
+    fi
+    
+    
+    
+
+
+done
+
+
+if [[ $count_sshport -ge 1 ]];then
+  exit
+fi
+
+
+}
+
+
+
+check_passwd(){
+
+
+
+for i in  $(seq 0 $((${#ip_list[*]}-1)))
+do
+
+
+    if [[ "${user_list[$i]}" == "null" ]];then
+      user_list[$i]="kunlun"
+    fi 
+   
+    if [[ "${basedir_list[$i]}" == "null" ]];then
+      basedir_list[$i]="/kunlun"
+      
+    fi
+ 
+    if [[ "${sshport_list[$i]}" == "null" ]];then
+      sshport_list[$i]=22
+    fi
+	    
+         
+         
+  expect <<EOF  >/dev/null 2>&1
+
+  spawn ssh -p${sshport_list[$i]} ${user_passwd[0]}@${ip_list[$i]} "echo Password is correct" 
+  expect {
+    "yes/no" { send "yes\n"; exp_continue }
+    "password:" {
+        send "${user_passwd[1]}\r"
+        exp_continue
+    }
+    "Password is correct" {
+        puts "Password is correct"
+        exit 0
+    }
+    timeout {
+        puts "Connection timed out or password is incorrect"
+        exit 1
+    }
+    eof {
+        puts "Failed to connect to remote host"
+        exit 1
+    }
+    "*assword incorrect*" {
+        puts "Password is incorrect"
+        exit 1
+    }
+}
+
+
+
+	expect eof
+
+EOF
+
+    
+  if [[ $? -ne 0 ]];then
+    echo -e "$COL_START${RED}${user_passwd[0]}@${ip_list[$i]}连接异常无法连接,请检用户名或密码$COL_END"
+    let count_passwd++
+   
+  fi  
+    
+
+
+done
+
+
+
+if [[ $count_passwd -ge 1 ]];then
+  exit
+fi
+
+
+
+}
+
+
+
+
+
+
+check_passwd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ install_klustron/show.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+COL_START='\e['
+COL_END='\e[0m'
+RED='31m'
+GREEN='32m'
+YELLOW='33m'
+config_json=$1
+VERSION=1.2.1
+
+
+<<!
+
+if ! nc -z  www.kunlunbase.com  80  &>/dev/null ;then   
+  echo  -e "$COL_START$RED当前主机网络异常$COL_END"
+  exit
+fi
+
+
+
+if [[ ! -d  /softwares   ]];then
+  sudo mkdir -p /softwares &>/dev/null  &&  cd /softwares &>/dev/null
+  if [[ $? -ne 0 ]];then
+     echo -e "$COL_START$RED请使用root用户或具有root权限创建/softwares目录$COL_END"
+     exit
+  fi
+    
+fi
+
+
+if [[ -d  /softwares/cloudnative   ]];then
+  echo -e "$COL_START$GREEN正在下载最新代码$COL_END"
+  cd /softwares  &&  sudo rm  -fr cloudnative &>/dev/null  &&  sudo git clone https://gitee.com/zettadb/cloudnative.git &>/dev/null
+  if [[ $? -eq 0 ]];then
+    echo -e "$COL_START$GREEN最新代码下载成功$COL_END"
+  else
+    echo -e "$COL_START$RED代码下载失败$COL_END"
+    exit
+  fi
+
+else
+  echo -e "$COL_START$GREEN正在下载最新代码$COL_END"
+  cd /softwares &&  sudo git clone https://gitee.com/zettadb/cloudnative.git  &>/dev/null
+  if [[ $? -eq 0 ]];then
+    echo -e "$COL_START$GREEN最新代码下载成功$COL_END"
+  else 
+    echo -e "$COL_START$RED代码下载失败$COL_END"
+    exit
+  fi
+
+fi
+
+
+
+if ! command -v figlet &>/dev/null  ;then
+  echo  -e "$COL_START$RED正在下载figlet命令 $COL_END"
+  
+  if [ -f "/etc/os-release" ]; then
+    source /etc/os-release
+    
+      if [[ "$ID" == "ubuntu" ]]; then
+        echo  -e "$COL_START${GREEN}OS is Ubuntu$COL_END"
+        sudo apt-get install -y figlet   &>/dev/null  
+        
+      elif [[ "$ID" == "centos" ]]; then
+        echo  -e "$COL_START${GREEN}OS is CentOS$COL_END"
+        sudo yum install -y figlet  &>/dev/null  
+        
+      else
+        echo  -e "$COL_START$RED未知系统$COL_END"
+        exit
+      fi
+        
+        
+  else
+    echo "os-release文件不存,未知系统"
+    exit
+
+  fi
+    
+
+fi
+
+
+!
+
+
+
+
+while true; do
+
+  echo -e "$COL_START$RED"
+    figlet  -c Klustron
+  echo -e "$COL_END"
+
+
+  echo  -e "$COL_START$GREEN
+  欢迎使用Klustron数据库,Klustron是一个分布式 HTAP数据库系统，聚焦于解决各行业的应用软件 Web 系统、
+  和 SaaS云服务在存储、管理和利用海量关系型数据,以及支撑高并发高负载的事务处理和数据读写服务,从而
+  为应用软件开发商、服务商和最终用户创造价值。
+  有关Klustron分布式数据详细信信息请查询泽拓科技(深圳)有限责任官方网站:https://www.kunlunbase.com/
+  
+  安装Klustron分布式数据库主要事项:
+1.安装脚本会自动下载最新代码,请使用root用户或具有root权限的用户运行此脚本
+2.代码下载路径为/softwares/cloudnative
+3.请切换到目录/softwares/cloudnative/cluster下面,vim cluster_template.json编辑配置文件
+  进行少量的配置(配置文件名称可以随意修改),修改对应IP即可(配置文件提供三个主机配置,采用对等配置达到节约资源的目的),
+  如果集群超过三个主机可以参考官方详细配置http://doc.klustron.com/zh/install_by_scripts.html
+  如果有疑问请及时联系泽拓科技售后人员快速帮您解决
+4.在目录/softwares/cloudnative/cluster下执行如: sudo sh install_klustron.sh cluster_template.json root_passwd进行集群安装
+  $COL_END"
+  
+  echo -e "$COL_START$RED退出请输入q|Q$COL_END" 
+  read -s input
+    
+    if [[ "$input" == "Q" || "$input" == "q" ]]; then
+        #echo "退出程序"
+        break
+    fi
+
+done
